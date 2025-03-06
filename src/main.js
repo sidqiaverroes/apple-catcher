0479016598a88ca49f93003e81156ef2d95143a0
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-03-06 15:50:07 +0700

    add score and timer

--- src/main.js
+++ src/main.js
@@ -15,36 +15,57 @@ class GameScene extends Phaser.Scene{
     this.cursor
     this.playerSpeed = speedDown+50
     this.target
-    this.points
+    this.points = 0
+    this.textScore
+    this.textTime
+    this.timedEvent
+    this.remainingTime
   }
 
   preload(){
     this.load.image("bg", "./public/assets/bg.png")
-    this.load.image("basket", "./public/assets/basket.png")
     this.load.image("apple", "./public/assets/apple.png")
+    this.load.image("basket", "./public/assets/basket.png")
   }
 
   create(){
     this.add.image(0,0,"bg").setOrigin(0,0)
+    this.target = this.physics.add.image(0,0,"apple").setOrigin(0,0)
     this.player = this.physics.add.image(0, 400,"basket").setOrigin(0,0)
+
     this.player.setImmovable(true)
     this.player.body.allowGravity = false
     this.player.setCollideWorldBounds(true)
     this.player.setSize(this.player.width*75/100, this.player.height/6).setOffset(this.player.width/10, this.player.height*90/100)
-
-    this.target = this.physics.add.image(0,0,"apple").setOrigin(0,0)
+    
     this.target.setMaxVelocity(0, speedDown)
 
     this.physics.add.overlap(this.target, this.player, this.targetHit, null, this)
 
     this.cursor = this.input.keyboard.createCursorKeys()
+
+    this.textScore = this.add.text(sizes.width*77/100, sizes.height/50, "Score: 0", {
+      font: "25px Arial",
+      fill: "#000000",
+    })
+    this.textTime = this.add.text(10, 10, "Remaining Time: 00", {
+      font: "25px Arial",
+      fill: "#000000",
+    })
+
+    this.timedEvent = this.time.delayedCall(30000, this.gameOver, [], this)
   }
   
   update(){
+    this.remainingTime = this.timedEvent.getRemainingSeconds()
+    this.textTime.setText(`Remaining Time: ${Math.round(this.remainingTime).toString()}`)
+
     if(this.target.y >= sizes.height)
     {
       this.target.setY(0);
       this.target.setX(this.getRandomX());
+      this.points = 0;
+      this.textScore.setText(`Score: ${this.points}`)
     }
 
     const {left, right} = this.cursor;
@@ -72,6 +93,11 @@ class GameScene extends Phaser.Scene{
     this.target.setY(0)
     this.target.setX(this.getRandomX())
     this.points++
+    this.textScore.setText(`Score: ${this.points}`)
+  }
+
+  gameOver(){
+    console.log("game over")
   }
 }
 
