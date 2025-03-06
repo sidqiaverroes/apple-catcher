86703f7541e24c61fa1bea1326b7783fa6b6b5b5
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-03-06 15:13:44 +0700

    game scene setup

--- src/main.js
+++ src/main.js
@@ -6,11 +6,31 @@ const sizes = {
   height: 500,
 }
 
+const speedDown = 300
+
+class GameScene extends Phaser.Scene{
+  constructor(){
+    super("scene-game")
+  }
+
+  preload(){}
+  create(){}
+  update(){}
+}
+
 const config = {
   type: Phaser.WEBGL,
   width: sizes.width,
   height: sizes.height,
-  canvas: gameCanvas
+  canvas: gameCanvas,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {y: speedDown},
+      debug: true
+    }
+  },
+  scene: [GameScene]
 }
 
 const game = new Phaser.Game(config)
