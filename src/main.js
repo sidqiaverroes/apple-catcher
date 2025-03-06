ed92b9e447812b9faf420d74a7c8afdd9edc16f3
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-03-06 15:19:53 +0700

    add player

--- src/main.js
+++ src/main.js
@@ -11,14 +11,19 @@ const speedDown = 300
 class GameScene extends Phaser.Scene{
   constructor(){
     super("scene-game")
+    this.player
   }
 
   preload(){
     this.load.image("bg", "./public/assets/bg.png")
+    this.load.image("basket", "./public/assets/basket.png")
   }
 
   create(){
     this.add.image(0,0,"bg").setOrigin(0,0)
+    this.player = this.physics.add.image(0, 400,"basket").setOrigin(0,0)
+    this.player.setImmovable(true)
+    this.player.body.allowGravity = false
   }
   
   update(){}
